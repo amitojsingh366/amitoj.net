8a871ad00a86d87d4de3e45215893d7da5e1b96b
Amitoj Singh <amitojsingh366@gmail.com>
2021-10-08 03:28:34 +0300

    fix paths

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -15,16 +15,10 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         <title>Amitoj Singh</title>
         <link rel="manifest" href="/manifest.json" />
         <link
-          href="/icons/favicon-16x16.png"
+          href="/favicon.ico"
           rel="icon"
           type="image/png"
           sizes="16x16"
-        />
-        <link
-          href="/icons/favicon-32x32.png"
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
         />
         <link rel="apple-touch-icon" href="/apple-icon.png"></link>
         <meta name="theme-color" content="#111827" />
