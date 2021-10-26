9ef83ec7fa9c7cd973099d9df6d3f83cd8b711ba
Amitoj Singh <amitojsingh366@gmail.com>
2021-10-26 19:52:51 +0300

    add reactStrictmode

--- next.config.js
+++ next.config.js
@@ -3,14 +3,20 @@ const withPWA = require('next-pwa')
 const runtimeCaching = require('next-pwa/cache')
 const { PHASE_DEVELOPMENT_SERVER } = require('next/constants')
 
+const base = {
+    reactStrictMode: true
+}
+
 module.exports = (phase, { defaultConfig }) => {
     if (phase === PHASE_DEVELOPMENT_SERVER) {
         return {
+            ...base
             /* development only config options here */
         }
     }
 
     return withPWA({
+        ...base,
         pwa: {
             dest: 'public',
             runtimeCaching,
