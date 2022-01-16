6fa4859559b4df2eb49b9bc1662f29e986141e02
Amitoj Singh <35400192+amitojsingh366@users.noreply.github.com>
2022-01-16 13:44:31 +0300

    update discord link

--- src/modules/home/home-page.tsx
+++ src/modules/home/home-page.tsx
@@ -24,7 +24,7 @@ export const HomePage: FC = () => {
             <div className="flex flex-row mt-20 space-x-10 md:space-x-16 h-10">
                 <IconLink Icon={FaGithub} link="https://github.com/amitojsingh366/" title="Check out my GitHub profile" />
                 <IconLink Icon={FaTwitter} link="https://twitter.com/amitojsingh366/" title="Check out my Twitter profile" />
-                <IconLink Icon={FaDiscord} link="https://discords.com/bio/p/amitoj" title="Contact me on Discord" />
+                <IconLink Icon={FaDiscord} link="https://discordapp.com/users/473965680857972757" title="Contact me on Discord" />
                 <IconLink Icon={IoMdMail} link="mailto:amitoj@amitoj.net" title="Contact me via E-Mail" />
                 <IconLink Icon={SiKofi} link="https://ko-fi.com/amitoj" title="Buy me a coffee 💞" />
                 <IconLink Icon={FaPaypal} link="https://paypal.me/amitoj366" title="Support me through PayPal 💞" />
